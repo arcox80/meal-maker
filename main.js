5835a0cd9857bfaed8f5fa4a2d0641e1e9e48e68
Andrew Cox <drew.r.cox@gmail.com>
2022-03-17 09:50:40 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,32 +1,27 @@
-let menu = {
+const menu = {
   courses: {
     appetizers: [],
     mains: [],
     desserts: []
   },
   addDishToCourse: function (courseName, dishName, dishPrice) {
-    let dish = {
+    const dish = {
       name: dishName,
       price: dishPrice
     }
     this.courses[courseName].push(dish);
   },
   getRandomDishFromCourse: function (courseName) {
-    let random = Math.floor(Math.random() * this.courses[courseName].length);
+    const random = Math.floor(Math.random() * this.courses[courseName].length);
     return this.courses[courseName][random];
   },
   generateRandomMeal: function () {
-    let appetizer = this.getRandomDishFromCourse('appetizers').name;
-    let main = this.getRandomDishFromCourse('mains').name;
-    let dessert = this.getRandomDishFromCourse('desserts').name;
+    const appetizer = this.getRandomDishFromCourse('appetizers');
+    const main = this.getRandomDishFromCourse('mains');
+    const dessert = this.getRandomDishFromCourse('desserts');      
+    const total = appetizer.price + main.price + dessert.price;
     
-    let appPrice = this.courses.appetizers.find(obj => obj.name === appetizer).price;
-    let mainPrice = this.courses.mains.find(obj => obj.name === main).price;
-    let dessertPrice = this.courses.desserts.find(obj => obj.name === dessert).price;
-    
-    let total = appPrice + mainPrice + dessertPrice;
-    
-    return `Your appetizer will be ${appetizer}, your main course will be ${main}, and your dessert will be ${dessert}. Your total price will be $${total}.`;
+    return `Your appetizer will be ${appetizer.name}, your main course will be ${main.name}, and your dessert will be ${dessert.name}. Your total price will be $${total}.`;
   }
 };
 
@@ -40,5 +35,5 @@ menu.addDishToCourse('desserts', 'pecan pie', 6);
 menu.addDishToCourse('desserts', 'cheese cake', 7);
 menu.addDishToCourse('desserts', 'ice cream', 5);
 
-let meal = menu.generateRandomMeal();
+const meal = menu.generateRandomMeal();
 console.log(meal);
